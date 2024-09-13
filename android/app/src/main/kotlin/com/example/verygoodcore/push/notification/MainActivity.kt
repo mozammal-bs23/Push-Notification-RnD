29a21efe7b07fbc21e0eeab09247b5a94cb3559b
Mozammal Hossain <mozammal.hossain@brainstation-23.com>
2024-09-13 18:02:45 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/push/notification/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.push_notification
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:push_notification/app/app.dart';
+import 'package:push_notification/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
